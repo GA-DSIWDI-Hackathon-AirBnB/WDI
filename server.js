b7d60c80718ce75a472ca9c84a724a1ee8b9be2f
matt keigwin <mkeigwin@bates.edu>
2016-12-08 09:37:22 -0500

    upstream pullin

--- server.js
+++ server.js
@@ -4,12 +4,10 @@ const logger      = require('morgan');
 const path        = require('path');
 const bodyParser  = require('body-parser');
 
-const isDev = !('NODE_ENV' in process.env) && require('dotenv').config() && true;
-
 const app         = express();
 const PORT        = process.argv[2] || process.env.PORT || 3000;
 
-app.use(logger(isDev ? 'dev' : 'common'));
+app.use(logger('dev'));
 
 app.use(express.static(path.join(__dirname, 'dist')));
 app.use(bodyParser.json());
